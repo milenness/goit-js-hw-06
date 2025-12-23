f092d4e42d3c259ed1d8c867cbfd5868048378c2
Milena Karpenko <milenness06@gmail.com>
2025-12-23 19:51:59 +0200

    uod hw

--- js/task-2.js
+++ js/task-2.js
@@ -1,24 +1,24 @@
 class Storage {
-    #items = [];
+  #items;
 
-    constructor(items) {
+  constructor(items) {
     this.#items = items;
   }
 
-    getItems() {
-        return this.#items;
-    }
+  getItems() {
+    return this.#items;
+  }
 
-    addItem(newItem) {
-        this.#items.push(newItem);
-    }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
 
-    removeItem(itemToRemove) {
+  removeItem(itemToRemove) {
     const itemIndex = this.#items.indexOf(itemToRemove);
     if (itemIndex !== -1) {
       this.#items.splice(itemIndex, 1);
     }
-}
+  }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
